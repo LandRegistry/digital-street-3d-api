b92da6d97fa46ccaba5112ef1775cd1784645a1d
Simon Chapman <simon.chapman@landregistry.gov.uk>
2017-09-28 10:05:50 +0100

    Allow application errors to be logged at a higher level

--- flask_skeleton_api/exceptions.py
+++ flask_skeleton_api/exceptions.py
@@ -3,21 +3,46 @@ import json
 
 
 class ApplicationError(Exception):
-    """This class is raised when the application identifies there's been a problem and the client should be informed.
+    """Use this class when the application identifies there's been a problem and the client should be informed.
 
-    Example: raise ApplicationError("Title number invalid", "E102", 400)
-    The handler method will then create the response body in a standard structure so clients
-    will always know what to parse.
+    Example: raise ApplicationError("Critical error", "DB")
+
+    or
+
+    raise ApplicationError("Title number invalid", "E102", http_code=400)
+
+    or
+
+    raise ApplicationError("Title number invalid", "E102", http_code=400, live_log=True)
     """
 
-    def __init__(self, message, code, http_code=500):
+    def __init__(self, message, code, http_code=500, live_log=False):
+        """Create an instance of the error.
+
+        Keyword arguments:
+
+        http_code - handler methods will use this to determine the http code to set in the returned Response
+        (default 500)
+
+        live_log - handler methods will use this to determine whether to log at debug or error. This parameter only
+        applies to expected application errors, as unhandled exceptions are always considered live-log worthy 
+        (default False)
+        """
         Exception.__init__(self)
         self.message = message
         self.http_code = http_code
         self.code = code
+        self.live_log = live_log
 
 
 def unhandled_exception(e):
+    """Handler method for exceptions that escape the route code without being caught.
+
+    A consistent JSON bodied response is returned.
+
+    Due to the lack of information available to provide to the client, and the fact there was clearly
+    no opportunity for cleanup or error handling in the processing code, this should be a never-event!
+    """
     current_app.logger.exception('Unhandled Exception: %s', repr(e))
     return Response(response=json.dumps({"error_message": "Internal Server Error", "error_code": "500"}),
                     mimetype='application/json',
@@ -25,7 +50,16 @@ def unhandled_exception(e):
 
 
 def application_error(e):
-    current_app.logger.debug('Application Exception: %s', repr(e), exc_info=True)
+    """Handler method for ApplicationErrors raised for to inform the client of a specific scenario.
+
+    A consistent JSON bodied response is returned.
+    """
+    # If requested, log at ERROR level
+    if e.live_log:
+        current_app.logger.exception('Application Exception (message: %s, code: %s): %s', e.message, e.code, repr(e))
+    else:
+        current_app.logger.debug('Application Exception (message: %s, code: %s): %s', e.message, e.code, repr(e),
+                                 exc_info=True)
     return Response(response=json.dumps({"error_message": e.message, "error_code": e.code}),
                     mimetype='application/json',
                     status=e.http_code)
